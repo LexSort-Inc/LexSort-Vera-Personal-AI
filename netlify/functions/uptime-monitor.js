a759f6ca74017f6196c7918a16e73dd6c1868893
justmegamingchamp-sketch <justme.gamingchamp@gmail.com>
2026-06-15 18:46:34 -0400

    fix: update uptime monitor release binary URLs to v1.1.0/v1.0.2

--- netlify/functions/uptime-monitor.js
+++ netlify/functions/uptime-monitor.js
@@ -1,11 +1,11 @@
 // netlify/functions/uptime-monitor.js
 
 const BINARY_URLS = {
-  mac_arm:   'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.0/LexSort.Personal.AI_1.0.0_aarch64.dmg',
-  mac_intel: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.0/LexSort.Personal.AI_1.0.0_x64.dmg',
-  windows:   'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.0/LexSort.Personal.AI_1.0.0_x64-setup.exe',
-  linux_app: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.0/LexSort.Personal.AI_1.0.0_amd64.AppImage',
-  linux_deb: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.0/LexSort.Personal.AI_1.0.0_amd64.deb'
+  mac_arm:   'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.1.0/LexSort.Personal.AI_1.1.0_aarch64.dmg',
+  mac_intel: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.1.0/LexSort.Personal.AI_1.1.0_x64.dmg',
+  windows:   'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.0.2/LexSort.Personal.AI_1.0.2_x64_en-US.msi',
+  linux_app: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.1.0/LexSort.Personal.AI_1.1.0_amd64.AppImage',
+  linux_deb: 'https://github.com/Lexsort-Core/LexSort-Vera-Personal-AI/releases/download/v1.1.0/LexSort.Personal.AI_1.1.0_amd64.deb'
 };
 
 exports.handler = async (event, context) => {
